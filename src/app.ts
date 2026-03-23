286a421bcbbd55f61beacd86a4a7784d7fb89b03
Vinay <vinay.kulkarni@continental-corporation.com>
2026-03-23 22:03:42 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,10 @@ import jobCardRoutes from "./routes/jobCard.routes";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }));
 app.use(helmet());
 app.use(morgan("dev"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
